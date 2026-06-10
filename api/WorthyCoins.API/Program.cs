9280b0b4e2f016bf5255cf650f716f299a9c4872
phdguigui <guiricardo@live.com>
2026-06-09 22:45:56 -0300

    CORS policy to accept all the requests (temporary while in dev)

--- api/WorthyCoins.API/Program.cs
+++ api/WorthyCoins.API/Program.cs
@@ -8,6 +8,15 @@ using WorthyCoins.Infrastructure.Identity.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
@@ -36,6 +45,8 @@ builder.Services
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
