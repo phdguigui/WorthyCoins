aa16c11bf479d815cfc10ca8833e16bf3b5f18ce
phdguigui <guiricardo@live.com>
2026-06-16 23:08:23 -0300

    Creating Result and Result<T> for services methods responses

---
+++ api/WorthyCoins.Application/Commons/Results/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace WorthyCoins.Application.Commons.Results
+{
+    public class Result
+    {
+        public bool Success { get; init; }
+        public string? ErrorCode { get; init; }
+
+        public static Result Ok() => new() { Success = true };
+
+        public static Result Fail(string errorCode)
+            => new()
+            {
+                Success = false,
+                ErrorCode = errorCode
+            };
+    }
+}
---
+++ api/WorthyCoins.Application/Commons/Results/ResultT.cs
@@ -0,0 +1,21 @@
+﻿namespace WorthyCoins.Application.Commons.Results
+{
+    public class Result<T> : Result
+    {
+        public T? Data { get; init; }
+
+        public static Result<T> Ok(T data)
+            => new()
+            {
+                Success = true,
+                Data = data
+            };
+
+        public static new Result<T> Fail(string errorCode)
+            => new()
+            {
+                Success = false,
+                ErrorCode = errorCode
+            };
+    }
+}
