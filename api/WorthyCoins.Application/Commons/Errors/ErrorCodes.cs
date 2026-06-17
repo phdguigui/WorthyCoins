064812e669c9e6e377f476eb44100bc69d66c758
phdguigui <guiricardo@live.com>
2026-06-16 23:08:55 -0300

    ErrorCodes to standarize Result and Resources

---
+++ api/WorthyCoins.Application/Commons/Errors/ErrorCodes.cs
@@ -0,0 +1,7 @@
+﻿namespace WorthyCoins.Application.Commons.Errors
+{
+    public static class ErrorCodes
+    {
+        public const string PasswordMissingUpperCase = "PASSWORD_MISSING_UPPER_CASE";
+    }
+}
