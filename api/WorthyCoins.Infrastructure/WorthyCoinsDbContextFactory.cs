a8916fcc9d154e4737e68afef2aca201520ea7a2
phdguigui <guiricardo@live.com>
2026-07-10 22:00:22 -0300

    Backend - Implementing User Secrets for default connection in connection strings

--- api/WorthyCoins.Infrastructure/WorthyCoinsDbContextFactory.cs
+++ api/WorthyCoins.Infrastructure/WorthyCoinsDbContextFactory.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace WorthyCoins.Infrastructure;
 
-public class WorthyCoinsDbContextFactory
-    : IDesignTimeDbContextFactory<WorthyCoinsDbContext>
+public class WorthyCoinsDbContextFactory(IConfiguration configuration)
+        : IDesignTimeDbContextFactory<WorthyCoinsDbContext>
 {
+    private readonly IConfiguration _configuration = configuration;
+
     public WorthyCoinsDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<WorthyCoinsDbContext>();
 
         optionsBuilder.UseNpgsql(
-            "*****"
+            _configuration.GetConnectionString("DefaultConnection")
         );
 
         return new WorthyCoinsDbContext(optionsBuilder.Options);
