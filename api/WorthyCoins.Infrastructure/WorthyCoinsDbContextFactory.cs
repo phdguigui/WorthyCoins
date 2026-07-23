3fda38ebfae10d69bb93467f354a070e087602c3
phdguigui <guiricardo@live.com>
2026-07-23 16:17:58 -0300

    Backend - Configuring DbContextFactory for the use of the user-secrets

--- api/WorthyCoins.Infrastructure/WorthyCoinsDbContextFactory.cs
+++ api/WorthyCoins.Infrastructure/WorthyCoinsDbContextFactory.cs
@@ -4,17 +4,29 @@ using Microsoft.Extensions.Configuration;
 
 namespace WorthyCoins.Infrastructure;
 
-public class WorthyCoinsDbContextFactory(IConfiguration configuration)
-        : IDesignTimeDbContextFactory<WorthyCoinsDbContext>
+public class WorthyCoinsDbContextFactory : IDesignTimeDbContextFactory<WorthyCoinsDbContext>
 {
-    private readonly IConfiguration _configuration = configuration;
-
     public WorthyCoinsDbContext CreateDbContext(string[] args)
     {
+        var basePath = Directory.GetCurrentDirectory();
+
+        var apiProjectPath = Path.Combine(basePath, "../WorthyCoins.API");
+        if (!Directory.Exists(apiProjectPath)) 
+        {
+            apiProjectPath = basePath;
+        }
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(apiProjectPath)
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddUserSecrets("ff120a12-cad3-4c27-8ac0-050e14d57794") // ID from WorthyCoins.API.csproj
+            .Build();
+
         var optionsBuilder = new DbContextOptionsBuilder<WorthyCoinsDbContext>();
 
         optionsBuilder.UseNpgsql(
-            _configuration.GetConnectionString("DefaultConnection")
+            configuration.GetConnectionString("DefaultConnection")
         );
 
         return new WorthyCoinsDbContext(optionsBuilder.Options);
