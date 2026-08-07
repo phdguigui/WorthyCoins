38bce08d3f74c556e0e1b63e678f3a09befe9e50
phdguigui <guiricardo@live.com>
2026-08-06 21:57:10 -0300

    Frontend - Modal select field component

---
+++ web/src/components/ModalSelectField/ModalSelectField.tsx
@@ -0,0 +1,16 @@
+import { InfiniteSelect } from "../Select/InfiniteSelect";
+import type { InfiniteSelectProps } from "../Select/InfiniteSelect";
+import styles from "../Modal/ModalFields.module.css";
+
+interface ModalSelectFieldProps extends Omit<InfiniteSelectProps, "className"> {
+  label: string;
+}
+
+export function ModalSelectField({ label, ...props }: ModalSelectFieldProps) {
+  return (
+    <div className={styles.field}>
+      <p className={styles.label}>{label}</p>
+      <InfiniteSelect className={styles.input} {...props} />
+    </div>
+  );
+}
