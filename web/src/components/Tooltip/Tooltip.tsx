84768b5b6bc16b196bf22d3b46a6fbe73f393dc6
phdguigui <guiricardo@live.com>
2026-08-10 19:13:12 -0300

    Frontend - Tooltip component

---
+++ web/src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+import styles from "./Tooltip.module.css";
+
+interface TooltipProps {
+  content: string;
+  children: ReactNode;
+}
+
+export function Tooltip({ content, children }: TooltipProps) {
+  return (
+    <div className={styles.tooltipWrapper}>
+      {children}
+      <span className={styles.tooltipText}>{content}</span>
+    </div>
+  );
+}
