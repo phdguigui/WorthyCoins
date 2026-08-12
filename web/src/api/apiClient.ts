98a6664cd2f21e5fedee752e21f303fb06ff4be9
phdguigui <guiricardo@live.com>
2026-08-11 23:38:47 -0300

    Frontend - API URL for production instance

--- web/src/api/apiClient.ts
+++ web/src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://localhost:7016/api",
+  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? "https://worthycoins-api.onrender.com/api" : "https://localhost:7016/api"),
   headers: {
     "Content-Type": "application/json",
   },
